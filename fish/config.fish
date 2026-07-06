e6aab84ad4b075a7b259cc97f592f7a314b6ab40
DinoMiHa <saintrapport@gmail.com>
2026-07-06 21:58:43 +0300

    just cleaning

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    set TERM xterm-256-color
 
     #ls
     alias ls='lsd'
@@ -16,26 +17,31 @@ if status is-interactive
     alias lgit='lazygit'
     alias sf='spf'
     if command -q bat
-        alias cat='bat'
+        alias bt='bat'
     else if command -q batcat
-        alias cat='batcat'
+        alias bt='batcat'
     end
     alias home='cd ~'
     alias dc='cd'
     alias hom='home'
     alias hm='home'
     alias rm='rm -rf'
-    alias gc='git clone'
     alias mk='mkdir'
     alias md='mkdir'
+
+    #git
+    alias gc='git clone'
+    alias ga='git add'
+    alias gal='ga *'
+    alias gcm='git commit -m'
+    alias gp='git push'
+    alias gin='git init'
 
     #neovim
     alias nv='nvim'
     alias nvf='nv ~/.config/fish/config.fish'
     alias nvn='nv ~/.config/niri/config.kdl'
     alias snv='sudo nvim'
-    #disks
-    alias startd='sudo ntfs-3g /dev/sdb1 /mnt/d'
 
     #packages
     if command -q pacman
@@ -47,18 +53,18 @@ if status is-interactive
         alias pacu='pac -U'
         alias pacupd='pac -Syu'
     else if command -q apt
-        alias apti='sudo apt install -y'
-        alias apts='sudo apt install'
-        alias aptr='sudo apt remove'
-        alias aptq='apt list --installed'
-        alias aptu='sudo apt update && sudo apt upgrade -y'
+        alias ai='sudo apt install -y'
+        alias as='sudo apt install'
+        alias ar='sudo apt remove'
+        alias aq='apt list --installed'
+        alias au='sudo apt update && sudo apt upgrade -y'
 
-        alias pac='sudo apt'
-        alias paci='apti'
-        alias pacs='apts'
-        alias pacr='aptr'
-        alias pacq='aptq'
-        alias pacupd='aptu'
+        alias apt='sudo apt'
+        alias apti='apti'
+        alias apts='apts'
+        alias aptr='aptr'
+        alias aptq='aptq'
+        alias aptu='aptu'
     end
 
     #flatpak
@@ -99,43 +105,22 @@ if status is-interactive
     #openclaude
     alias occ="$HOME/openclaude/bin/openclaude"
 
-    alias vts='sudo modprobe v4l2loopback devices=2 video_nr=0,1 card_label="0,1" exclusive_caps=1,1'
-    alias vts2='v4l2-ctl --list-devices && python ~/OpenSeeFace/facetracker.py'
-
     #ffinder
     function \\
         nohup firefox --new-window "https://www.google.com/search?q=$argv" >/dev/null
     end
 
-    alias swms='setsid start-hyprland & setsid startx -- :2 vt2 &'
-
     #setup
     command -q zoxide; and zoxide init fish | source
 
     #zapret
-    alias fix='~/zap/service.sh service start'
-    alias fixs='~/zap/service.sh service stop'
+    alias fix='zap/service.sh run -s "general (ALT).bat"'
 
     #shutdown
     alias shn='shutdown now'
 
-    export CLAUDE_CODE_USE_OPENAI=1
-    export OPENAI_API_KEY=sk-zxc
-    export OPENAI_BASE_URL=http://192.168.1.74:5001/
-    export OPENAI_MODEL=gpt-4o
-
-    alias kdeee='# Сначала только композитор
-WAYLAND_DISPLAY=wayland-2 \
-XDG_RUNTIME_DIR=/run/user/$(id -u) \
-dbus-run-session -- kwin_wayland --wayland-display wayland-2 &
-
-sleep 3
-
-# Потом Plasma поверх него
-WAYLAND_DISPLAY=wayland-2 \
-XDG_RUNTIME_DIR=/run/user/$(id -u) \
-QT_QPA_PLATFORM=wayland \
-plasmashell &'
+    #reboot
+    alias rbt='reboot'
 
     command -q fastfetch; and ff
 end
